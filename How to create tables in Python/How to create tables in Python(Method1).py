abd2969509d4b8dd8e35d20c0d2f5d85bfbfc312
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-10-08 12:34:46 +0530

    Create How to create tables in Python(Method1).py

---
+++ How to create tables in Python/How to create tables in Python(Method1).py
@@ -0,0 +1,8 @@
+from prettytable import PrettyTable
+
+Table = PrettyTable(["Name","Age","Occupation","City"])
+Table.add_row(["Mansi","21","Computer Engineer","Ahmedabad"])
+Table.add_row(["Mansi","21","Computer Engineer","Ahmedabad"])
+Table.add_row(["Mansi","21","Computer Engineer","Ahmedabad"])
+Table.add_row(["Mansi","21","Computer Engineer","Ahmedabad"])
+print(Table)
